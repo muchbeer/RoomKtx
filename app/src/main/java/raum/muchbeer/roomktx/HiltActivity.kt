5826f1fed46d4699b892e16c15827b121a70c9b2
muchbeer <gman2them@gmail.com>
2021-01-03 13:59:13 +0300

    corotine+kotlin+room+rest api using retrofit + Notification + Hilt Injection+ Testing Room Database also save the error  More than one file was found with OS independent path 'META-INF/ASF'
    Using constructor injection with Hilt and implement Interface

---
+++ app/src/main/java/raum/muchbeer/roomktx/HiltActivity.kt
@@ -0,0 +1,96 @@
+package raum.muchbeer.roomktx
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@AndroidEntryPoint
+class HiltActivity : AppCompatActivity() {
+
+    //this is called field injection
+    @Inject
+   lateinit var fielInjectMrMachibya: MrMachibya
+
+   //how to inject function using Interface
+   @Inject
+   lateinit var constructorFamily : Family
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hilt)
+
+        println("Familia ya John Machibya")
+        println(fielInjectMrMachibya.mtotoWaMachibya())
+        println(fielInjectMrMachibya.mjukuu())
+
+        println("---------")
+        println("Constructor Injection with Interface")
+        println("Familia ya Machibya")
+        println(constructorFamily.callMachibyaFamily())
+
+    }
+}
+
+class MrMachibya
+
+@Inject
+constructor(private val mjukuuWaMachibya:MrGeorge) {
+    fun mtotoWaMachibya() : String {
+        return "Mtoto wa tatu ni George"
+    }
+
+    fun mjukuu() : String {
+        return mjukuuWaMachibya.mtotoWaGeorge()
+    }
+}
+
+class MrGeorge
+@Inject
+constructor() {
+    fun mtotoWaGeorge() : String {
+        return "Mtoto wa George ni Gadiel"
+    }
+}
+
+class Family
+@Inject
+constructor( private val machibyaFamilyImpl: Machibya) {
+    fun callMachibyaFamily() : String {
+       return machibyaFamilyImpl.watotoWanne()
+    }
+}
+
+class MachibyaFamilyImpl
+@Inject
+constructor() : Machibya {
+    override fun watotoWanne(): String {
+       return "Watoto wa Machibya ni Jose, John, George, Evelyn"
+    }
+
+}
+interface Machibya {
+    fun watotoWanne() : String
+}
+
+//Using provides makes it very very easy
+//to implement MachibyaFamilyImpl
+//so let us do it
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class MachibyModule {
+
+    @Provides
+    @Singleton
+    fun machibyaFamilyImpl() : Machibya {
+        return MachibyaFamilyImpl()
+        //we don't have a constructor in MachibyaFamilyImpl() that is why we don't instatiate
+    }
+}
+
